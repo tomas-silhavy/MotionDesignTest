0774568936d560c5ea55b71ff9e196d5c980c8a5
Tomas <tomas.silhavy@gmail.com>
2017-04-16 21:55:23 +1200

    adding tests

--- src/test/java/com/motionDesign/PackTest.java
+++ src/test/java/com/motionDesign/PackTest.java
@@ -2,13 +2,17 @@ package com.motionDesign;
 
 import com.motionDesign.model.Item;
 import com.motionDesign.model.Pack;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
+import static com.motionDesign.PackPlanner.SortOrder.LONG_TO_SHORT;
+import static com.motionDesign.PackPlanner.SortOrder.SHORT_TO_LONG;
 import static org.testng.Assert.assertEquals;
 
 public class PackTest {
@@ -17,7 +21,7 @@ public class PackTest {
     private List<Item> items;
 
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp(){
         pp = new PackPlanner(PackPlanner.SortOrder.NATURAL, 2, new BigDecimal(40));
         items = new ArrayList<>();
@@ -28,25 +32,47 @@ public class PackTest {
 
     @Test
     public void testAddItemNatural() throws Exception {
-        List<Pack> packs = pp.packUp(new ArrayList<>(items));
+        List<Pack> packs = pp.packUp(items);
         packs.forEach(System.out::println);
         assertEquals(packs.size(), 16);
         assertEquals(packs.stream().map(Pack::getItems).flatMap(List::stream).mapToInt(Item::getQuantity).sum(), 31);
-
+        Set<Integer> set = new LinkedHashSet<>();
+        packs.stream().map(Pack::getItems).flatMap(List::stream).forEach(item -> set.add(item.getId()));
+        assertEquals(set.toArray(), new Integer[]{10, 20, 30});
     }
 
     @Test
     public void testAddItemLongToShort() throws Exception {
-
+        pp.setOrder(LONG_TO_SHORT);
+        pp.setMaxItemCount(4);
+        pp.setMaxPackWeight(new BigDecimal(16));
+        List<Pack> packs = pp.packUp(items);
+        packs.forEach(System.out::println);
+        assertEquals(packs.size(), 12);
+        assertEquals(packs.stream().map(Pack::getItems).flatMap(List::stream).mapToInt(Item::getQuantity).sum(), 31);
+        Set<Integer> set = new LinkedHashSet<>();
+        packs.stream().map(Pack::getItems).flatMap(List::stream).forEach(item -> set.add(item.getId()));
+        assertEquals(set.toArray(), new Integer[]{20, 10, 30});
     }
 
     @Test
     public void testAddItemShortToLong() throws Exception {
-
+        pp.setOrder(SHORT_TO_LONG);
+        pp.setMaxItemCount(12);
+        pp.setMaxPackWeight(new BigDecimal(34));
+        List<Pack> packs = pp.packUp(items);
+        packs.forEach(System.out::println);
+        assertEquals(packs.size(), 6);
+        assertEquals(packs.stream().map(Pack::getItems).flatMap(List::stream).mapToInt(Item::getQuantity).sum(), 31);
+        Set<Integer> set = new LinkedHashSet<>();
+        packs.stream().map(Pack::getItems).flatMap(List::stream).forEach(item -> set.add(item.getId()));
+        assertEquals(set.toArray(), new Integer[]{30, 10, 20});
     }
 
-    @Test
-    public void testGetTotalItemCount() throws Exception {
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testTooHeavyItem() throws Exception {
+        items.add(new Item(40, 8, 40, new BigDecimal(100)));
+        pp.packUp(items);
     }
 
 }
